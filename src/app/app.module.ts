bd9d4060f8f7c1238177e527e0eb99a1269400a7
Katia Duarte <katiaipduarte@hotmail.com>
2020-02-16 16:57:17 +0000

    Add new style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule }  from '@angular/material/grid-list';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { GalleryItemComponent } from './gallery/gallery-item/gallery-item.component';
@@ -14,16 +17,19 @@ import { GalleryItemComponent } from './gallery/gallery-item/gallery-item.compon
 @NgModule({
    declarations: [
       AppComponent,
+      HeaderComponent,
       BannerComponent,
       GalleryComponent,
-      GalleryItemComponent      
+      GalleryItemComponent,
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       BrowserAnimationsModule,
       MatGridListModule,
-      LayoutModule
+      LayoutModule,
+      MatIconModule,
+      MatButtonModule
    ],
    providers: [],
    bootstrap: [
